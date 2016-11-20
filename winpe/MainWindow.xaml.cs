5ba9637ec6e7466f3e99410157400c625d0c9691
heidarj <heidarj@hraunteigur5.com>
2016-11-20 18:47:29 +0000

    init

---
+++ winpe/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using WpfAppBar;
+
+namespace winpe {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class PixieLauncher : Window {
+        public PixieLauncher() {
+            InitializeComponent();
+        }
+
+        bool isDocked = false;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            //Dock();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e) {
+            Dock();
+        }
+
+        public void Dock() {
+            if (isDocked) {
+                AppBarFunctions.SetAppBar(this, ABEdge.None);
+                isDocked = false;
+                menuItemDock.Header = "Dock";
+            }
+            else {
+                AppBarFunctions.SetAppBar(this, ABEdge.Right, image1, true);
+                isDocked = true;
+                menuItemDock.Header = "Undock";
+            }
+        }
+
+        private void image1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            image1.Opacity = 70;
+        }
+
+        private void image1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            image1.Opacity = 100;
+            Dock();
+        }
+    }
+}
---
+++ winpe/menu.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using winpe;
+
+namespace PixieLauncher {
+    /// <summary>
+    /// Interaction logic for menu.xaml
+    /// </summary>
+    public partial class menu : UserControl {
+
+        static string windrive = System.IO.Path.GetPathRoot(Environment.SystemDirectory);
+        static string toolsDir = windrive + @"Tools\";
+
+        public menu() {
+            InitializeComponent();
+            string[] tools = Directory.GetFiles(toolsDir);
+            Console.WriteLine(tools[0]);
+
+            foreach (string tool in tools) {
+                FileInfo fi = new FileInfo(tool);
+                if ((fi.Extension == ".exe") || (fi.Extension == ".lnk") || (fi.Extension == ".bat")) {
+                    Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(tool);
+                    shortcut a = new winpe.shortcut();
+                    a.image.Source = loadBitmap(icon.ToBitmap());
+                    a.label.Content = System.IO.Path.GetFileNameWithoutExtension(tool);
+                    a.MouseDown += new MouseButtonEventHandler(button_Click);
+                    a.path = tool;
+                    a.Margin = new Thickness(6, 6, 6, 6);
+
+                    wrapPanel.Children.Add(a);
+                }
+            }
+        }
+
+        [DllImport("gdi32")]
+        static extern int DeleteObject(IntPtr o);
+
+        public static BitmapSource loadBitmap(Bitmap source) {
+            IntPtr ip = source.GetHbitmap();
+            BitmapSource bs = null;
+            try {
+                bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(ip,
+                   IntPtr.Zero, Int32Rect.Empty,
+                   BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally {
+                DeleteObject(ip);
+            }
+
+            return bs;
+        }
+
+        void button_Click(object sender, RoutedEventArgs e) {
+            shortcut b = sender as shortcut;
+            Process proc = new Process();
+            proc.StartInfo.FileName = b.path;
+            proc.Start();
+        }
+    }
+}
